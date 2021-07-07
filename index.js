118be9f63e01868454317b33fc6ebac272025252
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-07 20:26:55 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 console.clear()
-let str = "Graham Robert Brooks"
+let str = 'You do not know what you do not know until you know.';
+let substr = 'know';
+
+let count = 0;
+
+let index = str.indexOf(substr);
+while (index !== -1) {
+    count++;
+    index = str.indexOf(substr, index + 1);
+}
+
+console.log(count);
