a37e4d9229b33dd49ad0645ed4d59a619817f39a
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-07 22:28:07 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.clear()
+
+function exists(value, array) {
+    return array.some(e => e === value);
+}
+let marks = [4, 5, 7, 9, 10, 2];
+console.log(exists(3, marks));
+console.log(exists(10, marks))
