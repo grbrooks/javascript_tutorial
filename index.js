a025e0e46cc04e43299fda9457190dbd6688e908
Graham Brooks <grahambrooks@hotmail.co.uk>
2021-07-08 00:57:01 +0100

    --- Auto-Git Commit ---

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 console.clear()
 
-function exists(value, array) {
-    return array.some(e => e === value);
-}
-let marks = [4, 5, 7, 9, 10, 2];
-console.log(exists(3, marks));
-console.log(exists(10, marks))
+let employees = [
+    { name: 'John', salary: 90000, hireDate: "July 1, 2010" },
+    { name: 'David', salary: 75000, hireDate: "August 15, 2009" },
+    { name: 'Ana', salary: 80000, hireDate: "December 12, 2011" }
+];
+employees.sort(function(x, y) {
+    return x.salary - y.salary;
+});
+console.table(employees)
+employees.sort(function(x, y) {
+    let a = x.name.toUpperCase(),
+        b = y.name.toUpperCase();
+    return a == b ? 0 : a > b ? 1 : -1;
+});
+
+console.table(employees);
